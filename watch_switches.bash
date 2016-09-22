ebea4e16032ba2c604ac548da9bacd1977dcdd54
citueda <ueda@cit-ar-lab.tech>
2016-09-22 19:00:24 +0900

    Add operations on push

--- watch_switches.bash
+++ watch_switches.bash
@@ -4,5 +4,11 @@ exec 2> /tmp/watch_button.log
 
 while sleep 1 ; do
 	BUTTONS=$(cat /dev/rtswitch{0,1,2} | tr -d '\n' | tr '10' '01')
-	echo ${BUTTONS}
-done > /home/ubuntu/a
+	case "$BUTTONS" in
+	"100" ) echo 1 > /dev/rtmotoren0 ;;
+	"010" ) /home/ubuntu/run ;;
+	"001" ) echo 0 > /dev/rtmotoren0 ;;
+	"111" ) shutdown -h now ;;
+	* ) echo ERROR ;;
+	esac
+done
