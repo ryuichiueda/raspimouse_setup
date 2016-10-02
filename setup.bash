2501463751660d4e6c24e68442d9c2dbff20f737
Ryuichi Ueda <ryuichiueda@gmail.fcom>
2016-10-02 13:31:47 +0900

    Test

--- setup.bash
+++ setup.bash
@@ -2,7 +2,7 @@
 
 exec 2> /tmp/setup.log
 
-cd /home/ubuntu/RaspberryPiMouse/src/drivers/
+cd ~/RaspberryPiMouse/src/drivers/
 /sbin/insmod rtmouse.ko
 
 sleep 1                   #すぐにはデバイスファイルができないので待つ
